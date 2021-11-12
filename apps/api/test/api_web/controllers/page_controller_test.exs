263dfe84c4df7b8a15345bc9bf8a2630f895d77e
Kevin Mathew <kevinam99@gmail.com>
2021-11-12 22:22:51 +0530

    `mix format`

--- apps/api/test/api_web/controllers/page_controller_test.exs
+++ apps/api/test/api_web/controllers/page_controller_test.exs
@@ -4,6 +4,7 @@ defmodule ApiWeb.PageControllerTest do
   setup %{conn: conn} do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
   end
+
   test "GET /api", %{conn: conn} do
     conn = get(conn, "/api")
     assert response(conn, 200)
