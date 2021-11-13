1c45f82a39dfcec4e1998cf2c6cca3f056b65fce
Kevin Mathew <kevinam99@gmail.com>
2021-11-14 01:13:03 +0530

    Remove dev table

--- config/dev.exs
+++ config/dev.exs
@@ -1,15 +1,5 @@
 use Mix.Config
 
-# Configure your database
-config :dbstore, Dbstore.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "dbstore_dev",
-  hostname: "localhost",
-  show_sensitive_data_on_connection_error: true,
-  pool_size: 10
-
 # import Config
 
 config :api, ApiWeb.Endpoint,
