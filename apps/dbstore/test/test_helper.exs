649ea4562d06c5bc80917ba05b963d9659746d85
Kevin Mathew <kevinam99@gmail.com>
2021-11-13 16:14:05 +0530

    Add test for `device` model

--- apps/dbstore/test/test_helper.exs
+++ apps/dbstore/test/test_helper.exs
@@ -1,1 +1,12 @@
-ExUnit.start()
+defmodule TestHelper do
+
+  ExUnit.start()
+
+  alias Dbstore.Device
+
+  def get_device() do
+  device = %{name: "device", active: false}
+  Device.changeset(%Device{}, device)
+  end
+
+end
